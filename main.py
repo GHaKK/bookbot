b6065d48a1399b2db5f7175caf06d643cd34aaf8
Tommy Larsen <tommy.larsen@ghakk.se>
2025-04-06 13:56:24 +0200

    seems to be kinda done

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from stats import get_num_words
 from stats import get_num_char
 from stats import get_sorted_list
+import sys
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 
-def main():
-    str_text = get_book_text("books/frankenstein.txt")
+def main(book):
+    str_text = get_book_text(book)
     num_words = get_num_words(str_text)
     num_char = get_num_char(str_text)
     sorted_list = (get_sorted_list(get_num_char(str_text)))
@@ -21,4 +23,19 @@ def main():
         print(f"{sorted_list[i]['char']}: {sorted_list[i]['num']}")
     print("============= END ===============")
     
-main()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("""
+Usage: python3 main.py <path_to_book>
+""")
+        sys.exit(1)
+    try:
+        get_book_text(sys.argv[1])
+    except FileNotFoundError:
+        print(sys.exit("file not found"))
+    except Exception as e:
+        print(e)
+
+    main(sys.argv[1])
